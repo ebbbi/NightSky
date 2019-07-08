860f8a1feed4ce6ebcd747fb699d334880a1f1d0
wonderjungin <50102738+wonderjungin@users.noreply.github.com>
2019-07-08 20:51:54 +0900

    Update models.py

--- NightSky/main/models.py
+++ NightSky/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+    body = models.TextField(default='')
+    writer = models.TextField(default='')
+    pub_date = modles.DateTimeField(default='')
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    published_date = models.DateTimeField(
+        blank=True, null=True
+    )
