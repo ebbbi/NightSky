2124e2ff0463da0dcba1e6bcc92d9a54b0b5bfff
Minju Lee <bolting0330@gmail.com>
2019-07-27 17:40:09 +0900

    fix manage.py

--- NightSky-master/NightSky/NightSky/settings.py
+++ NightSky-master/NightSky/NightSky/settings.py
@@ -25,8 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main.apps.MainConfig',
-    'accounts.apps.AccountsConfig',
+    'main.apps.MainConfig'
 ]
 
 MIDDLEWARE = [
@@ -93,9 +92,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'ko-kr'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Seoul'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
@@ -108,7 +107,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
--- NightSky-master/NightSky/main/forms.py
+++ NightSky-master/NightSky/main/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 from .models import Post, Comment
 
-class PostForm(forms.ModelForm):
+class PostForm(forms.modelForm):
     class Meta:
         model = Post
-        fields=['body', 'emotion',]
+        fields=['body','writer','author','pub_date',]
 
 
 class CommentForm(forms.ModelForm):
