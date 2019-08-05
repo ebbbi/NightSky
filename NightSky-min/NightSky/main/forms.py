0ae6b680677929bddd4c68d6d0dc975afcd31133
baruna0330 <bolting0330@naver.com>
2019-08-05 14:34:27 +0900

    minju commit

---
+++ NightSky-min/NightSky/main/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from .models import Post, Comment
+
+class PostForm(forms.ModelForm):
+
+    Ch=(('1', '별'), ('2', '먹구름'),)
+    emotion=forms.ChoiceField(choices=Ch, widget=forms.RadioSelect)
+    
+    class Meta:
+        model = Post
+        fields=['body', 'emotion',]
+
+"""
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model=Comment
+        fields=['content',]
+"""
+
+class CommentForm(forms.ModelForm):
+    #text = forms.TextInput(label = '댓글')
+
+    class Meta:
+        model = Comment
+        fields = ['text']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['text'].label = "댓글"
